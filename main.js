08b0fe5601b1614c781a87c266545fbed696a715
Fan Weiguang <lahalaha90@gmail.com>
2017-08-22 22:50:42 +0800

    bug fix

--- main.js
+++ main.js
@@ -3,15 +3,26 @@ const express = require('express');
 const app = express();
 const port = process.env.port || process.env.PORT || 8000
 
-const mysql = require('mysql');
-const config = require('./config');
+// const mysql = require('mysql');
+// const config = require('./config');
 
-const pool = mysql.createPool(config.MYSQL_OPTIONS);
+// const pool = mysql.createPool(config.MYSQL_OPTIONS);
 
-pool.getConnection(function(err, connection) {
-  if (err) throw err;
-  console.log("Database Connected: %s", connection);
-});
+function loadJsonFromFile(jsonPath, req, res) {
+  fs.readFile(jsonPath, function(err, data) {
+    if (err) {
+      res.end(err.message);
+    } else {
+      res.writeHead(200, { "Content-Type": "application/json" });
+      res.end(data.toString());
+    }
+  });
+}
+
+// pool.getConnection(function(err, connection) {
+//   if (err) throw err;
+//   console.log("Database Connected: %s", connection);
+// });
 
 app.use(express.static(__dirname));
 
@@ -20,7 +31,7 @@ app.get('/', function (req, res) {
 });
 
 app.get('/questions', function (req, res) {
-  loadJsonFromFile("./resources/mockdata/questions.json", req, res);
+  loadJsonFromFile("./resources/mock-data/questions.json", req, res);
 });
 
 app.listen(port, function () {
