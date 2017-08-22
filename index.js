b33466e8ba1ba6bf65e80b9a5c0abf341909d310
Han <leook0209@gmail.com>
2017-08-22 13:30:00 +0800

    Set up mysql database

--- index.js
+++ index.js
@@ -1,7 +1,16 @@
 const express = require('express');
 const app = express();
-
 const port = process.env.port || process.env.PORT || 8000
+
+const mysql = require('mysql');
+const config = require('./config');
+
+const mysqlCon = mysql.createConnection(config.MYSQL_OPTIONS);
+
+mysqlCon.connect(function(err) {
+  if (err) throw err;
+  console.log("Connected!");
+});
 
 app.get('/', function (req, res) {
   res.send('Hello World!')
