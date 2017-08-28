1a56e214a105f09c64ef556668b14218b0be9e75
Jun Wei <ng.95.jun.wei@gmail.com>
2017-08-28 18:57:04 +0800

    changed logic to accept userid, users as input. also changed test

--- logic/matchalgotest.js
+++ logic/matchalgotest.js
@@ -1,7 +1,9 @@
 var Algo = require('./matchalgo.js');
 
-var user = {"userid" : 1, "attributes": [1,2,3,4,5]};
-var rest = [{"userid" : 2, "attributes": [1,1,1,1,1]},
+var user = 1;
+
+var users = [{"userid" : 1, "attributes": [1,2,3,4,5]},
+{"userid" : 2, "attributes": [1,1,1,1,1]},
 {"userid" : 3, "attributes": [2,2,2,2,2]},
 {"userid" : 4, "attributes": [3,3,3,3,3]},
 {"userid" : 5, "attributes": [4,4,4,4,4]},
@@ -11,6 +13,6 @@ var rest = [{"userid" : 2, "attributes": [1,1,1,1,1]},
 {"userid" : 9, "attributes": [2,4,1,3,5]},
 {"userid" : 10, "attributes": [1,2,3,4,5]}];
 
-console.log(Algo.MostDifferent(user, rest));
+console.log(Algo.MostDifferent(user, users));
 
-console.log(Algo.MostSimilar(user, rest));
+console.log(Algo.MostSimilar(user, users));
