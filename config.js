db8c3c169b46118dbfe29ab7faf801ded3c14361
Han <leook0209@gmail.com>
2017-09-02 21:48:17 +0800

    edit config.js

--- config.js
+++ config.js
@@ -2,7 +2,7 @@ module.exports = {
     DBOPTIONS: {
         connectionLimit: 10,
         host: "localhost",
-        user: root,
+        user: "root",
         password: process.env.MYSQL_PASSWORD,
         database: "Teamker"
     }
